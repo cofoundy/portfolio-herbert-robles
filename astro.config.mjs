9325c382b61aef2dd3f0672cbebba0dba96aef1c
Melissa Iman Noriega <melissa.iman.n@uni.pe>
2026-05-05 16:05:07 -0500

    sync: source code at deploy time

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://herbert-robles.cofoundy.dev",
+  site: "https://robles-retina.com",
   vite: {
     plugins: [tailwindcss()],
   },
